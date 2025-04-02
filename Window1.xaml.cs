1374e38a89e154f5b32f01782df8dcac40e57c5e
孙子茗 <bxweihang2010@outlook.com>
2025-04-02 10:57:23 +0800

    添加项目文件。

---
+++ Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            DataContext = new Window1ViewModel();
+        }
+
+        
+    }
+
+
+}
---
+++ Window1ViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace WpfApp1
+{
+    public partial class Window1ViewModel : ObservableObject
+    {
+
+        private ObservableCollection<Person> _people;
+
+        public ObservableCollection<Person> People
+        {
+            get => _people;
+            set => SetProperty(ref _people, value);  // SetProperty 自动触发通知
+        }
+
+        public Window1ViewModel()
+        {
+            People = new ObservableCollection<Person>
+            {
+                new Person { Name = "1AL1", Pe = 30 },
+                new Person { Name = "1AL2", Pe = 25 },
+                new Person { Name = "1AL3", Pe = 30 },
+                new Person { Name = "1AL4", Pe = 25 },
+                new Person { Name = "1AL5", Pe = 30 },
+                new Person { Name = "1AL2", Pe = 25 },
+                new Person { Name = "1AL1", Pe = 30 },
+                new Person { Name = "1AL2", Pe = 25 },
+                new Person { Name = "1AL1", Pe = 30 },
+                new Person { Name = "1AL1", Pe = 30 },
+                new Person { Name = "1AL2", Pe = 25 },
+                new Person { Name = "1AL2", Pe = 25 },
+            };
+        }
+
+
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public double Pe { get; set; }
+    }
+  
+    
+}
